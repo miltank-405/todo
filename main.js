596ae1c7390c63f247b40650dcc0031a0e14f5b0
Yoko Kamata <koitn.saru@gmail.com>
2021-08-11 18:41:12 +0900

    inputの内容

--- main.js
+++ main.js
@@ -2,12 +2,22 @@
 // 	- input要素から内容を取得
 // 	- enterを押して送信されたのを理解させる
 // 	- つまり？formの送信を監視できればいいのでは？
+// 	- #js-listに入力したタスクを追加して表示
+// formに入力 --(submit)送信(addEventListener)--> 内容を取得して表示
+
+// #js-formの要素をGETだぜ！
+const formElement = document.querySelector('#js-form');
+// submitのイベントが発生した時に内容をGET
+const inputElement = document.querySelector('#js-text-input')
+
+// formからきたsubmitをGETだぜ！
+formElement.addEventListener('submit', (event) => {
+})
+
+
+// お試し
+// console.log(inputElement.value);
 
 
 
 
-
-
-
-
-// バッテン押したら消える
